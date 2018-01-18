ad354059f6e6cea5db46c2671f9c90b39cc1bcfd
Kazuhiro Takahashi <bluesky.hope.happy@gmail.com>
2018-01-18 13:48:57 +0000

    finish kadai tasklist

---
+++ tasks_controller.rb
@@ -0,0 +1,3 @@
+rename::             cannot open `rename:' (No such file or directory)
+task_contoroller.rb: cannot open `task_contoroller.rb' (No such file or directory)
+/dev/stdin:                   very short file (no magic)
